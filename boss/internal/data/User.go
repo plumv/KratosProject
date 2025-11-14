f49e195ff268cd7a4268a095c8c040a0ec272f07
lichuan <2382394203@qq.com>
2025-11-14 14:17:23 +0800

    完善boss模块的创建步骤-boss/internal

--- boss/internal/data/User.go
+++ boss/internal/data/User.go
@@ -7,25 +7,17 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 )
 
-func NewUserRepo(data *Data, logger log.Logger) biz.UserRepo {
-	return &userRepo{
-		data: data,
-		log:  log.NewHelper(logger),
-	}
-}
-
-// userRepo 是biz.UserRepo接口的实现，实现了数据库的具体操作
 type userRepo struct {
 	data *Data
 	log  *log.Helper
 }
 
-func (r *userRepo) FindAll(ctx context.Context) ([]*biz.User, error) {
-	panic("implement me")
-}
-
-func (r *userRepo) FindPage(ctx context.Context) ([]*biz.User, error) {
-	panic("implement me")
+// NewUserRepo .
+func NewUserRepo(data *Data, logger log.Logger) biz.UserRepo {
+	return &userRepo{
+		data: data,
+		log:  log.NewHelper(logger),
+	}
 }
 
 func (r *userRepo) Save(ctx context.Context, g *biz.User) (*biz.User, error) {
@@ -37,5 +29,13 @@ func (r *userRepo) Update(ctx context.Context, g *biz.User) (*biz.User, error) {
 }
 
 func (r *userRepo) FindByID(context.Context, int64) (*biz.User, error) {
+	return nil, nil
+}
+
+func (r *userRepo) ListByHello(context.Context, string) ([]*biz.User, error) {
+	return nil, nil
+}
+
+func (r *userRepo) ListAll(context.Context) ([]*biz.User, error) {
 	return nil, nil
 }
